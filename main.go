71ea7cd796f577984522e90b9c3f9cf6c3d14798
sla-ppy <sla-ppy-pm@protonmail.com>
2025-02-24 19:16:24 +0100

    succesful session/init call, response needs fixing

--- main.go
+++ main.go
@@ -3,13 +3,8 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 )
 
 type sessionInit struct {
@@ -20,7 +15,8 @@ type sessionInit struct {
 	Phone   string `json:"phone"`
 }
 
-const server_url = "https://sign-test.comnica.com"
+const baseUrl = "https://sign-test.comnica.com/api/session"
+const authToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOlsiY3JlYXRlX3Nlc3Npb24iXSwiaXNzIjoic2lnbi1iYWNrZW5kLXRlc3QiLCJleHAiOjE3NDM4NzQzNDYsImlhdCI6MTczOTU1NDM0Niwic3ViIjoiY29tcGFueTpiYWNrZW5kLWRldmVsb3Blci5jb21uaWNhLmlkIn0.x9qB3JtDtl-cGp9ijyjaL-lVuRSQsOk-KVibU8p3eyk"
 
 func main() {
 	// 0. initialize data to serialize
@@ -31,89 +27,36 @@ func main() {
 		Email:   "jackyissocial@gmail.com",
 		Phone:   "36203925891",
 	}
-
+	
 	// 1. serialize from Go data to JSON
 	jsonData, err := json.Marshal(data)
 	if err != nil {
-		fmt.Printf("*ERR*: Could not marshal json: %s\n", err)
+		fmt.Printf("*ERR*: Could not marshal JSON: %s\n", err)
 		return
 	}
-	fmt.Printf("Client: JSON data prepared:\n %s\n", jsonData)
+	fmt.Printf("Client: JSON data prepared:\n%s\n", jsonData)
 
-	// send POST
-	go func() {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Printf("Server: %s /\n", r.Method)
-			fmt.Printf("Server: query id: %s\n", r.URL.Query().Get("id"))
-			// header information helps with identifying problems with the server
-			fmt.Printf("Server: content-type: %s\n", r.Header.Get("content-type"))
-			fmt.Printf("Server: headers:\n")
-			for headerName, headerValue := range r.Header {
-				fmt.Printf("\t%s = %s\n", headerName, strings.Join(headerValue, ", "))
-			}
+	// prepare post for /session/init
+	req, err := http.NewRequest("POST", baseUrl+"/init", bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/json")   // tell the server we are sending json data
+	req.Header.Add("Accept", "application/json")         // accept tells the server we are expecting json back
+	req.Header.Add("Authorization", "Bearer "+authToken) // auth token is a must for all post requests for us
 
-			reqBody, err := io.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("*ERR*: Server could not read request body: %s\n", err)
-			}
-			fmt.Printf("Server: request body: %s\n", reqBody)
-
-			fmt.Fprintf(w, `{"message": "hello!"}`)
-		})
-
-		server := http.Server{
-			Addr:    fmt.Sprintf(":%d", serverPort),
-			Handler: mux,
-		}
-
-		if err := server.ListenAndServe(); err != nil {
-			if !errors.Is(err, http.ErrServerClosed) {
-				fmt.Printf("*ERR*: Error running HTTP server: %s\n", err)
-			}
-		}
-	}()
-
-	// sleep till server is rdy after POST, get ready for GET
-	time.Sleep(100 * time.Millisecond)
-
-	// []byte since encoding/json in go returns []byte
-	jsonBody := []byte(`{"client_message": "hello, server!"}`)
-	// wrapper for jsonBody
-	bodyReader := bytes.NewReader(jsonBody) // exists so jsonBody can be the value as the request body, since http.Request needs a value that is io.Reader
-
-	// create HTTP request
-	requestURL := fmt.Sprintf(server_url) // dont specify port, we dont care
-	req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+	// actually send post
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("*ERR*: client could not create post request: %s\n", err)
-		os.Exit(1)
+		fmt.Println("*ERR*: Establishing /session/init failed! Status: ", resp.Status)
+		panic(err)
 	}
-	// Header contains information on what the content type is, this is what i'll need to set to .pdf somehow
-	// application/pdf !!
-	req.Header.Set("Content-Type", "application/json")
+	defer resp.Body.Close()
 
-	client := http.Client{
-		Timeout: 30 * time.Second,
-	}
+	// GET response
+	fmt.Println("Response Status:", resp.Status)
+	fmt.Println("Response Headers:", resp.Header)
+	fmt.Println("Response Body:", resp.Body)
+	fmt.Println("Response Body:", resp)
 
-	// send the actual request
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("*ERR*: client couldn't send the request: %s\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Client: got response!\n")
-	fmt.Printf("Client: status code: %d\n", res.StatusCode)
-
-	// read the HTTP response body
-	// ReadAll() reads from io.Reader, returns data as []byte or error
-	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Printf("*ERR*: client could not read response body: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Client: response body:\n %s\n", resBody)
-
+	const newAuthToken = ""
+	const sessionId = ""
 }
