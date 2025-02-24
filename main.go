d527b8f60e5d807ca8d5d8d75158f2be03bc32ed
sla-ppy <sla-ppy-pm@protonmail.com>
2025-02-24 20:34:23 +0100

    succesful /session/init call!

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-type sessionInit struct {
+type sessionInitRequest struct {
 	Company string `json:"company"` // string and int cant have value: nil, since "" and 0 are their empty values
 	CaseId  string `json:"case_id"` // it can either be type or nil, we need to make it a reference
 	Name    string `json:"name"`    // omitempty handles whether string is omitted or not
-	Email   string `json:"email"`   // of omitempty is added and the string is empty, it wont be output on the console!
+	Email   string `json:"email"`   // of omitempty is added and the string is empty, it won't be output on the console!
 	Phone   string `json:"phone"`
 }
 
@@ -19,17 +20,17 @@ const baseUrl = "https://sign-test.comnica.com/api/session"
 const authToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOlsiY3JlYXRlX3Nlc3Npb24iXSwiaXNzIjoic2lnbi1iYWNrZW5kLXRlc3QiLCJleHAiOjE3NDM4NzQzNDYsImlhdCI6MTczOTU1NDM0Niwic3ViIjoiY29tcGFueTpiYWNrZW5kLWRldmVsb3Blci5jb21uaWNhLmlkIn0.x9qB3JtDtl-cGp9ijyjaL-lVuRSQsOk-KVibU8p3eyk"
 
 func main() {
-	// 0. initialize data to serialize
-	data := &sessionInit{
+	// initialize data to serialize
+	requestData := &sessionInitRequest{
 		Company: "backend-developer.comnica.id",
 		CaseId:  "sla-ppy",
 		Name:    "Miklos Vida",
 		Email:   "jackyissocial@gmail.com",
 		Phone:   "36203925891",
 	}
-	
-	// 1. serialize from Go data to JSON
-	jsonData, err := json.Marshal(data)
+
+	// serialize from Go data to JSON
+	jsonData, err := json.Marshal(requestData)
 	if err != nil {
 		fmt.Printf("*ERR*: Could not marshal JSON: %s\n", err)
 		return
@@ -42,7 +43,7 @@ func main() {
 	req.Header.Add("Accept", "application/json")         // accept tells the server we are expecting json back
 	req.Header.Add("Authorization", "Bearer "+authToken) // auth token is a must for all post requests for us
 
-	// actually send post
+	// make POST request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -51,12 +52,36 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	// GET response
-	fmt.Println("Response Status:", resp.Status)
-	fmt.Println("Response Headers:", resp.Header)
-	fmt.Println("Response Body:", resp.Body)
-	fmt.Println("Response Body:", resp)
+	var initResult struct {
+		SessionId   string `json:"session_id"`
+		BearerToken string `json:"bearer_token"`
+	}
 
-	const newAuthToken = ""
-	const sessionId = ""
+	// check if OK, proceed with unmarshalling
+	if resp.StatusCode == http.StatusOK {
+		fmt.Printf("Succesful /session/init call! Status Code is OK: %d\n", resp.StatusCode)
+
+		// read response body
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("*ERR*: Cannot read response body: %s\n", err)
+		}
+
+		// unmarshal from go to json
+		//var result map[string]interface{}
+		err = json.Unmarshal(body, &initResult)
+		if err != nil {
+			fmt.Printf("*ERR*: Unmarshalling was unsuccesful: %s\n", err)
+		}
+
+		fmt.Printf("{\"session_id\":\"%s\",\"bearer_token\":\"%s\"}\n", initResult.SessionId, initResult.BearerToken)
+	} else {
+		fmt.Printf("*ERR*: Status Code is not OK: %d\n", resp.StatusCode)
+	}
+
+	// response from server after POST
+	//fmt.Println("Response Status:", resp.Status)
+	//fmt.Println("Response Headers:", resp.Header)
+	//fmt.Println("Response Body:", resp.Body)
+	//fmt.Println("Response Body:", resp)
 }
